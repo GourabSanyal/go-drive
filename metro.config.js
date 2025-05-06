48e579cd00affe6d24977ec5c65c3fe678e4c09c
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-05-07 00:22:14 +0530

    feat: enhance module resolution with path aliases in metro.config.js and tsconfig.json

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,14 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+config.resolver.alias = {
+  '@': path.resolve(__dirname),
+  '@types': path.resolve(__dirname, 'src/types'),
+  '@enums': path.resolve(__dirname, 'src/enums'),
+  '@components': path.resolve(__dirname, 'components'),
+};
 
 module.exports = config;
