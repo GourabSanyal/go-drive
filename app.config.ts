c91678ac164d9df9a9a65bf0455b1a602faa9722
takshakmudgal <takshakmudgal@gmail.com>
2025-05-17 01:49:47 +0530

    feat: integrate Mapbox for enhanced location services, update app configuration, and improve DriverActiveRideScreen with map functionality

--- app.config.ts
+++ app.config.ts
@@ -1,84 +1,97 @@
-import { ConfigContext, ExpoConfig } from "expo/config"
+import { ConfigContext, ExpoConfig } from "expo/config";
 
-const IS_DEV = process.env.APP_VARIANT === 'development';
-const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
 
 const getUniqueIdentifier = () => {
   if (IS_DEV) {
-    return 'com.yourname.stickersmash.dev';
+    return "com.yourname.stickersmash.dev";
   }
 
   if (IS_PREVIEW) {
-    return 'com.yourname.stickersmash.preview';
+    return "com.yourname.stickersmash.preview";
   }
 
-  return 'com.yourname.stickersmash';
+  return "com.yourname.stickersmash";
 };
 
 const getAppName = () => {
   if (IS_DEV) {
-    return 'StickerSmash (Dev)';
+    return "StickerSmash (Dev)";
   }
 
   if (IS_PREVIEW) {
-    return 'StickerSmash (Preview)';
+    return "StickerSmash (Preview)";
   }
 
-  return 'StickerSmash: Emoji Stickers';
+  return "StickerSmash: Emoji Stickers";
 };
-
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-    "name": getAppName(),
-    "slug": "go-cabs-and-drive",
-    "version": "0.0.1",
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "drive",
-    "userInterfaceStyle": "automatic",
-    "newArchEnabled": true,
-    "jsEngine": "hermes",
-    "splash": {
-      "image": "./assets/images/splash-icon.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+  name: getAppName(),
+  slug: "go-cabs-and-drive",
+  version: "0.0.1",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "drive",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  jsEngine: "hermes",
+  splash: {
+    image: "./assets/images/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: getUniqueIdentifier(),
+    jsEngine: "hermes",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
     },
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": getUniqueIdentifier(),
-      "jsEngine": "hermes"
-    },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
+    package: getUniqueIdentifier(),
+    jsEngine: "hermes",
+  },
+  developmentClient: {
+    silentLaunch: true,
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "@rnmapbox/maps",
+      {
+        RNMapboxMapsDownloadToken:
+          "sk.eyJ1IjoidGFrc2hha211ZGdhbCIsImEiOiJjbWFyNmMzNnkwMmF0MmxzMms3NjczdG5oIn0.cjRtcsNQ25-5c0vWTDG6rg",
+        RNMapboxMapsVersion: "11.8.0", // Or your desired version, 11.x is recommended
       },
-      "package": getUniqueIdentifier(),
-      "jsEngine": "hermes"
-    },
-    "developmentClient": {
-      "silentLaunch": true
-    },
-    "web": {
-      "bundler": "metro",
-      "output": "static",
-      "favicon": "./assets/images/favicon.png"
-    },
-    "plugins": [
-      "expo-router"
     ],
-    "experiments": {
-      "typedRoutes": true
-    },
-    "extra": {
-      "router": {
-        "origin": false
+    [
+      "expo-location",
+      {
+        locationWhenInUsePermission:
+          "Show current location on map for navigation.",
       },
-      "eas": {
-        "projectId": "fdd3b266-be2b-4347-9598-c6373b304ac4"
-      }
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    router: {
+      origin: false,
     },
-    "owner": "go-cabs"
-  }
-)
+    eas: {
+      projectId: "fdd3b266-be2b-4347-9598-c6373b304ac4",
+    },
+  },
+  owner: "go-cabs",
+});
