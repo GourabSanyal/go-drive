6adb4e1cacaef46287ccaba6b992fe216846de23
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-09-03 14:30:48 +0530

    chore: update app configuration for Google services, add GoogleService-Info.plist, and enhance Podfile for static frameworks

--- app.config.ts
+++ app.config.ts
@@ -4,15 +4,7 @@ const IS_DEV = process.env.APP_VARIANT === "development";
 const IS_PREVIEW = process.env.APP_VARIANT === "preview";
 
 const getUniqueIdentifier = () => {
-  if (IS_DEV) {
-    return "com.yourname.stickersmash.dev";
-  }
-
-  if (IS_PREVIEW) {
-    return "com.yourname.stickersmash.preview";
-  }
-
-  return "com.yourname.stickersmash";
+  return "com.go.drive";
 };
 
 const getAppName = () => {
